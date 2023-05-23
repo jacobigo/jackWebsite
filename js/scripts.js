0d0f3c709e6145fc8adffcb277dd9a53994c33e6
jacobigo <jacobigo1@icloud.com>
2023-05-23 03:00:03 -0700

    added slideshow, finished pages, polished

--- js/scripts.js
+++ js/scripts.js
@@ -52,3 +52,17 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+
+//image slideshow
+window.addEventListener("DOMContentLoaded", function(e) {
+
+
+    var stage = document.getElementById("stage");
+    var fadeComplete = function(e) { stage.appendChild(arr[0]); };
+    var arr = stage.getElementsByTagName("a");
+    for(var i=0; i < arr.length; i++) {
+      arr[i].addEventListener("animationend", fadeComplete, false);
+    }
+
+  }, false);
